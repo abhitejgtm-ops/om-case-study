daec78979b907ffa9ee3973cbb3009f48d75a559
Abhitej Yadav <abhitejgtm@gmail.com>
2026-08-15 14:34:03 +0530

    Infrastructure configuration

--- infrastructure-as-code/main.tf
+++ infrastructure-as-code/main.tf
@@ -1,11 +1,20 @@
-
-
 variable "files" {
   default = 5
 }
 
+locals {
+  file_names = {
+    file0 = 0
+
+    file2 = 2
+    file3 = 3
+    file4 = 4
+  }
+}
+
 resource "local_file" "foo" {
-  count    = var.files
-  content  = "# Some content for file ${count.index}"
-  filename = "file${count.index}.txt"
+  for_each = local.file_names
+
+  content  = "# Some content for file ${each.value}"
+  filename = "${each.key}.txt"
 }
